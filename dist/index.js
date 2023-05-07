76ce62d698486d4646129bc8e52089a4f997978d
Jeff <cfjlu@connect.ust.hk>
2023-05-07 10:55:50 +0800

    platform created

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,19 @@
 var config = {
     type: Phaser.CANVAS,
-    width: 800,
+    width: 1000,
     height: 600,
     physics: {
-        default: 'arcade', //default 是 Arcade physics
+        default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 800 },
             debug: false
         }
     },
     scene: {
+        key: 'main',
         preload: preload,
         create: create,
+        update: update,
     }
 };
 
@@ -21,16 +23,31 @@ function init() {
 
 function preload() {
     this.load.image('background', 'assets/background.jpg');
+    this.load.image('base_tiles','assets/Mario1/Tilesets/OverWorld.png');
+    this.load.tilemapTiledJSON('map', 'assets/platform.json');
+    this.load.spritesheet('cat', 'assets/cat.png', {frameWidth: 50, frameHeight: 50});
 }
 
-var platforms;
+var map;
+var player;
+var cursors;
+var groundLayer, coinLayer;
+var text;
 function create() {
-    this.add.image(300, 400, 'background')
+    map = this.make.tilemap({key: 'map', tileHeight: 16, tileWidth: 16});
+    const tileset = map.addTilesetImage('3', 'base_tiles');
+    groundLayer = map.createLayer('Tile Layer 1', tileset, 0, 0);
+    // this.add.image(300, 400, 'background');
+
     // platforms = this.physics.add.staticGroup();
     // platforms.create(400, 568, 'ground').setScale(2).refreshBody();
     // platforms.create(600, 400, 'ground');
     // platforms.create(50, 250, 'ground');
     // platforms.create(750, 220, 'ground');
+}
+
+function update(){
+
 }
 
 var game = new Phaser.Game(config);
