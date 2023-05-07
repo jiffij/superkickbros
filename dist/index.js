a8ee3dcd3cc905d0c241a172852486e49a5b8cc8
Jeff <cfjlu@connect.ust.hk>
2023-05-07 17:09:18 +0800

    cat fix

--- dist/index.js
+++ dist/index.js
@@ -25,8 +25,8 @@ function preload() {
     this.load.image('background', 'assets/background.jpg');
     this.load.image('base_tiles','assets/Mario1/Tilesets/OverWorld.png');
     this.load.tilemapTiledJSON('map', 'assets/platform.json');
-    this.load.spritesheet('cat1', 'assets/cat1.png', {frameWidth: 50, frameHeight: 50});
-    this.load.spritesheet('cat2', 'assets/cat2.png', {frameWidth: 50, frameHeight: 50});
+    this.load.spritesheet('cat1', 'assets/cat1.png', {frameWidth: 64, frameHeight: 64});
+    this.load.spritesheet('cat2', 'assets/cat2.png', {frameWidth: 64, frameHeight: 64});
 }
 
 var map;
@@ -34,19 +34,22 @@ var player;
 var cursors;
 var groundLayer, coinLayer;
 var text;
+var body;
 function create() {
     map = this.make.tilemap({key: 'map', tileHeight: 16, tileWidth: 16});
     const tileset = map.addTilesetImage('3', 'base_tiles');
     groundLayer = map.createLayer('Tile Layer 1', tileset, 0, 0);
     groundLayer.setCollisionByExclusion([-1]);
-    // console.log(groundLayer.width, groundLayer.height);
     this.physics.world.bounds.width = groundLayer.width;
     this.physics.world.bounds.height = groundLayer.height;
-    player = this.physics.add.sprite(16, 400, 'cat1');
-    player.setBounce(0.2);
+    player = this.physics.add.sprite(16, 16, 'cat1');
+
+    player.body.setSize(16,16);
+    player.body.setOffset(26,40);
+    player.setBounce(0.2, 0.2);
     player.setCollideWorldBounds(true);
     this.physics.add.collider(groundLayer, player);
-    player.setScale(1,1);
+
     cursors = this.input.keyboard.addKeys(
         'W,A,S,D'
     );
@@ -58,8 +61,10 @@ function update(){
     }else if(cursors.S.isDown){
 
     }else if(cursors.A.isDown){
+        player.setScale(-1,1);
         player.body.setVelocityX(-200);
     }else if(cursors.D.isDown){
+        player.setScale(1,1);
         player.body.setVelocityX(200);
     }
 }
